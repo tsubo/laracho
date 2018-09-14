b438cbbe58746a508b80e7b3ae0811992047f35d
tsubo <ktsubota@gmail.com>
2018-09-14 11:25:06 +0900

    add contact

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,7 @@ module.exports = {
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Laravel 入門', link: '/tutorial/' },
+      { text: 'Contact', link: '/contact' },
       // { text: 'Laravel の仕組み', link: '/architecture/' },
       // { text: 'パッケージ紹介', link: '/packages/' },
       // { text: 'API 開発入門', link: '/api/' },
@@ -12,12 +13,7 @@ module.exports = {
       // { text: 'Q&A', link: 'https://ja.stackoverflow.com/questions/tagged/laravel' }
     ],
     sidebar: {
-      '/tutorial/': [
-        '',
-        'install',
-        'display_pages',
-        'create_view',
-      ],
-    }
-  }
+      '/tutorial/': ['', 'install', 'display_pages', 'create_view'],
+    },
+  },
 }
---
+++ docs/contact.md
@@ -0,0 +1,16 @@
+# コンタクト
+
+<form name="contact" method="POST" netlify>
+  <p>
+    <label>お名前: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>メールアドレス: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>メッセージ: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">送信</button>
+  </p>
+</form>
